0a80ef355e440e7bf21213672da82c6e1d1ca5e2
aschaaf <schaaf1091@gmail.com>
2019-04-06 15:45:11 -0600

    finished portfolio

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-    M.AutoInit();
-	
+	M.AutoInit();
+
 	// SETUP VARIABLES
 	// ============================================
 	var home = $('#homepage');
@@ -8,27 +8,28 @@ $(document).ready(function () {
 	var skills = $('#skillspage');
 	var port = $('#portpage');
 	var contact = $('#contactpage');
-	
+
 	var hLink = $('#home');
 	var aLink = $('#about');
 	var sLink = $('#skills');
 	var pLink = $('#portfolio');
 	var cLink = $('#contact');
-	
+
 	// MAIN PROCESS
 	// ============================================	
 	home.addClass('animated rollIn fast').show();
 	$('.title').addClass('animated rubberBand').show();
 	$('.tagline').addClass('animated fadeIn').show();
+	$('.big-logo').addClass('animated fadeIn').show();
 	setTimeout(changeFont, 1400);
-	
+
 	// MOUSE OVER EVENTS
 
 	// Toggle nav text
-	$('.nav-icon').mouseover(function(event) {
+	$('.nav-icon').mouseover(function (event) {
 		var title = $(this).data('name');
 		$(this).html(title).addClass('menu-text').removeClass('material-icons');
-	}).mouseout(function(){
+	}).mouseout(function () {
 		if ($(this).hasClass('selected')) {
 			title = $(this).data('name');
 			$(this).html(title);
@@ -37,12 +38,12 @@ $(document).ready(function () {
 			$(this).html(icon).removeClass('menu-text').addClass('material-icons');
 		};
 	});
-	
+
 	// Toggle sidenav text
-	$('.nav-name').mouseover(function(event) {
+	$('.nav-name').mouseover(function (event) {
 		var title = $(this).data('line');
 		$(this).html(title);
-	}).mouseout(function(){
+	}).mouseout(function () {
 		if ($(this).hasClass('selected')) {
 			title = $(this).data('line');
 			$(this).html(title);
@@ -51,26 +52,28 @@ $(document).ready(function () {
 			$(this).html(name);
 		};
 	});
-	
+
 	// Animate project image
-	$('.responsive-img').mouseover(function(event) {
+	$('.responsive-img').mouseover(function (event) {
 		$(this).addClass('animated jello faster').removeClass('lightSpeedIn slow slower fast faster delay-1s delay-2s').css('opacity', '1');
-	}).mouseout(function(){
+	}).mouseout(function () {
 		$(this).removeClass('jello').css('opacity', '.8');
 	});
 
 	// CLICK EVENTS
-	
+
 	// Change nav item text
-	$('.nav-icon').on('click', function() {;
+	$('.nav-icon').on('click', function () {
+		;
 		$('.nav-icon').removeClass('selected');
 		revertNavItems();
 		var title = $(this).data('name');
 		$(this).addClass('selected').html(title).addClass('menu-text').removeClass('material-icons');
 	});
-	
+
 	// Change sidebar item text
-	$('.nav-name').on('click', function() {;
+	$('.nav-name').on('click', function () {
+		;
 		$('.nav-name').removeClass('selected');
 		$('#about-side').html($('#about-side').data('name'));
 		$('#skills-side').html($('#skills-side').data('name'));
@@ -79,18 +82,18 @@ $(document).ready(function () {
 		var title = $(this).data('line');
 		$(this).addClass('selected').html(title);
 	});
-	
+
 	$('#home').on('click', function () {
 		$('.nav-icon').removeClass('selected');
 		revertNavItems();
 		$('section').hide();
 		home.show();
-	});	
-	
-	$('#skillspage a').on('click', function () {
-		$('.test').addClass('grey-text').html('This was created using JavaScript.').toggle();
 	});
-	
+
+	$('#skillspage a').on('click', function () {
+		$('.test').addClass('grey-text').html(`<img src="assets/images/oreo.jpg"><p>This is my doggo, his name is Oreo.</p>`).toggle();
+	});
+
 	// Display modal of project
 	$(document).on('click', '.responsive-img', function () {
 		var title = $(this).data('title');
@@ -100,16 +103,16 @@ $(document).ready(function () {
 		var modal = $(this).data('target');
 		showModal(title, link, link2, url, modal);
 	});
-	
+
 	// Display referenced section
 	$(document).on('click', '.nav-icon, .nav-name', showSection);
 
 	// FUNCTIONS
 	// ============================================
-	function showSection () {
+	function showSection() {
 		var linkName = $(this).data('name');
 		$('section').addClass('animated fast').hide();
-		
+
 		if (linkName === 'Home') {
 			home.addClass('bounceInDown').show();
 		};
@@ -122,29 +125,32 @@ $(document).ready(function () {
 		if (linkName === 'Portfolio') {
 			port.addClass('bounceInDown').show();
 		};
+		if (linkName === 'Contact') {
+			contact.addClass('bounceInDown').show();
+		};
 	};
-	
-	function revertNavItems () {
+
+	function revertNavItems() {
 		aLink.html(aLink.data('icon')).addClass('material-icons').removeClass('menu-text');
 		sLink.html(sLink.data('icon')).addClass('material-icons').removeClass('menu-text');
 		pLink.html(pLink.data('icon')).addClass('material-icons').removeClass('menu-text');
 		cLink.html(cLink.data('icon')).addClass('material-icons').removeClass('menu-text');
 	};
-	
-	function showModal (title, link, link2, url, modal) {	
+
+	function showModal(title, link, link2, url, modal) {
 		var content = $('<div>').addClass('modal-content center-align');
 		var img = $(`<img src="${url}" class="z-depth-4"><br>`);
-		var t = $('<h4>').text(title);
+		var t = $('<h4>').text(title).addClass('fun-font');
 		var a = $(`<a href="${link}" target="_blank" style="margin-left: 15px"></a>`).addClass('waves-effect waves-light btn modal-btn');
 		a.html('Demo<i class="fas fa-laptop" style="margin-left: 10px"></i>');
 		var a2 = $(`<a href="${link2}" target="_blank" style="margin-left: 15px"></a>`).addClass('waves-effect waves-light btn modal-btn');
 		a2.html('View Code<i class="fas fa-laptop-code" style="margin-left: 10px"></i>');
 		content.append(img, a2, a);
 		content.prepend(t);
-		 $(`#${modal}`).html(content);
+		$(`#${modal}`).html(content);
 	};
-	
-	function changeFont () {
+
+	function changeFont() {
 		$('.title').addClass('fun-font');
 	};
 
